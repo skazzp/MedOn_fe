a93292004010ed9d5e0ec817393a5b6e108409e5
skazzp <skazzp@gmail.com>
2023-03-30 16:19:50 +0300

    fix absolute imports

--- apps/medon-fe/src/app/app.tsx
+++ apps/medon-fe/src/app/app.tsx
@@ -1,5 +1,5 @@
+import ExamplePage from 'pages/ExamplePage';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import ExamplePage from '../pages/ExamplePage';
 
 function App() {
   return (
