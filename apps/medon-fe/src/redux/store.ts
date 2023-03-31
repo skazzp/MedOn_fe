3571698cc3a34b52ba2b68fcd39e4abf01d8f2e9
Matheus <matheus-rodrigues37@live.com>
2023-03-31 16:15:21 -0300

    update store.ts to allow use of RTK Query

--- apps/medon-fe/src/redux/store.ts
+++ apps/medon-fe/src/redux/store.ts
@@ -1,12 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
 import counterReducer from './features/counter/counterSlice';
+import { fetchDataApi } from './features/fetchData/fetchData';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [fetchDataApi.reducerPath]: fetchDataApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(fetchDataApi.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
+
+setupListeners(store.dispatch);
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
