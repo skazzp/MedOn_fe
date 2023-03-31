78c641ab2e99f87e71830d8fd1ef63fd26e6c2cd
Matheus <matheus-rodrigues37@live.com>
2023-03-31 15:28:47 -0300

    feat: create ExampleCounter

---
+++ apps/medon-fe/src/components/ExampleCounter/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  width: 100%;
+`;
