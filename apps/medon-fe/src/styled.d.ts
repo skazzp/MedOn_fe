22929d52b88858508e3d7a98e3bec37a69e235e5
Matheus <matheus-rodrigues37@live.com>
2023-04-01 12:21:53 -0300

    feat: create declaration styled.d.ts

---
+++ apps/medon-fe/src/styled.d.ts
@@ -0,0 +1,38 @@
+// import original module declarations
+import 'styled-components';
+
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      red_300: string;
+      red_500: string;
+      red_700: string;
+      yellow_300: string;
+      yellow_500: string;
+      yellow_700: string;
+      green_300: string;
+      green_500: string;
+      green_700: string;
+      blue_100: string;
+      blue_300: string;
+      blue_500: string;
+      blue_700: string;
+      blue_900: string;
+      gray_200: string;
+      gray_300: string;
+      gray_400: string;
+      gray_500: string;
+      gray_600: string;
+      gray_700: string;
+      white: string;
+      black: string;
+    };
+    typography: {
+      fontFamily: {
+        roboto: string;
+        sf_pro_text: string;
+      };
+    };
+  }
+}
