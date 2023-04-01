9b75e9e274ac9733e9a074be272e7cffec215b9f
Matheus <matheus-rodrigues37@live.com>
2023-04-01 12:08:32 -0300

    style: add color pallette and initial typography

---
+++ apps/medon-fe/src/styles/theme.ts
@@ -0,0 +1,32 @@
+export const theme = {
+  colors: {
+    red_300: '#FEEAE8',
+    red_500: '#E9380D',
+    red_700: '#660F0A',
+    yellow_300: '#FFF9E9',
+    yellow_500: '#FFC107',
+    yellow_700: '#EEB200',
+    green_300: '#CEDEC4',
+    green_500: '#00A344',
+    green_700: '#00662B',
+    blue_100: '#E2EDFD',
+    blue_300: '#408AFD',
+    blue_500: '#1B6CE0',
+    blue_700: '#0D3666',
+    blue_900: '#081972',
+    gray_200: '#F2F2F2',
+    gray_300: '#E5E5E5',
+    gray_400: '#CCCCCC',
+    gray_500: '#B3B3B3',
+    gray_600: '#808080',
+    gray_700: '#585858',
+    white: '#ffffff',
+    black: '#000000',
+  },
+  typography: {
+    fontFamily: {
+      roboto: 'Roboto, sans-serif',
+      sf_pro_text: 'SF Pro Text, sans-serif',
+    },
+  },
+};
