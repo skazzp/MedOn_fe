7306ba48928a12afc8826227df33d574eb2ac65d
Matheus <matheus-rodrigues37@live.com>
2023-04-01 14:03:13 -0300

    fix: add styled component provider

--- apps/medon-fe/src/main.tsx
+++ apps/medon-fe/src/main.tsx
@@ -3,23 +3,27 @@ import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './translation/i18next';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 
 import App from './app/app';
 import { store } from './redux/store';
 import { GlobalStyle } from './styles/global';
-
 import 'assets/fonts/sf-pro-font/sf-font-face.css';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <GlobalStyle />
-        <App />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <GlobalStyle />
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   </StrictMode>
 );
