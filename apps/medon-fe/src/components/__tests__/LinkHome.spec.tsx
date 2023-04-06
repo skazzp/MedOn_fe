7d1549dd160771743cb8734bbab0e38e4c4c67e3
Matheus <matheus-rodrigues37@live.com>
2023-04-06 14:55:41 -0300

    MED-48 test: add tests for the components

---
+++ apps/medon-fe/src/components/__tests__/LinkHome.spec.tsx
@@ -0,0 +1,50 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import { ReactNode } from 'react';
+import LinkHome from '../LinkHome';
+import { theme } from '../../styles/theme';
+
+const Wrapper = ({ children }: { children: ReactNode }) => (
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </BrowserRouter>
+);
+
+describe('LinkHome component', () => {
+  it('renders a link with the provided text color and background color', () => {
+    render(
+      <Wrapper>
+        <LinkHome to="/" textcolor="#fff" bgcolor="#000">
+          Home
+        </LinkHome>
+      </Wrapper>
+    );
+    const link = screen.getByRole('link');
+    expect(link).toHaveStyle('color: #fff');
+    expect(link).toHaveStyle('background-color: #000');
+  });
+
+  it('renders a full-width link when isfullwidth prop is true', () => {
+    render(
+      <Wrapper>
+        <LinkHome to="/" isfullwidth="true">
+          Home
+        </LinkHome>
+      </Wrapper>
+    );
+    const link = screen.getByRole('link');
+    expect(link).toHaveStyle('width: 100%');
+  });
+
+  it('renders a link with the provided to prop', () => {
+    render(
+      <Wrapper>
+        <LinkHome to="/">Home</LinkHome>
+      </Wrapper>
+    );
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('href', '/');
+  });
+});
