27185db912364b9b5e25f66490b8f92d6c173bd0
skazzp <skazzp@gmail.com>
2023-04-11 17:56:26 +0300

    MED-38 fix options

--- apps/medon-fe/src/utils/countries/countryOptions.tsx
+++ apps/medon-fe/src/utils/countries/countryOptions.tsx
@@ -1,6 +1,6 @@
 import countries from 'utils/countries/countries.json';
 
-export const countryOptions = countries.map((country) => {
-  const option = { value: country.name, label: country.name };
-  return option;
-});
+export const countryOptions = countries.map((country) => ({
+  value: country.name,
+  label: country.name,
+}));
--- apps/medon-fe/src/utils/timezones/timezoneOptions.tsx
+++ apps/medon-fe/src/utils/timezones/timezoneOptions.tsx
@@ -1,12 +1,9 @@
 import timezones from 'utils/timezones/timezones.json';
 
-export const timezoneOptions = timezones.map((timezone) => {
-  const option = {
-    value: timezone.text,
-    label: timezone.text,
-    offset: timezone.offset,
-  };
-  return option;
-});
+export const timezoneOptions = timezones.map((timezone) => ({
+  value: timezone.text,
+  label: timezone.text,
+  offset: timezone.offset,
+}));
 
 export const DEFAULT_TIMEZONE = '(UTC) Coordinated Universal Time';
