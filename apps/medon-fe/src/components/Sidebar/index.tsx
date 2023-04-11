2627b144fffd93ee3b663e5e8e96809bffb0554e
eduardojigub <eduardobrunoit@gmail.com>
2023-04-11 12:09:29 -0300

    feat: dumb sidebar

--- apps/medon-fe/src/components/Sidebar/index.tsx
+++ apps/medon-fe/src/components/Sidebar/index.tsx
@@ -4,9 +4,7 @@ export default function Sidebar() {
   return (
     <SidebarContainer>
       <MenuList>
-        <MenuItem>Dashboard</MenuItem>
         <MenuItem>Profile</MenuItem>
-        <MenuItem>Settings</MenuItem>
       </MenuList>
     </SidebarContainer>
   );
