4f99e1b5df904742213b907d37a3fdff7d3073e7
eduardojigub <eduardobrunoit@gmail.com>
2023-04-11 21:54:57 -0300

    feat: using antD for sidebar dumb

--- apps/medon-fe/src/components/Sidebar/styles.ts
+++ apps/medon-fe/src/components/Sidebar/styles.ts
@@ -1,25 +1,5 @@
 import styled from 'styled-components';
 
-export const SidebarContainer = styled.div`
-  width: 200px;
-  height: 100%;
-  background-color: #E2EDFD;
-  position: fixed;
-  top: 0;
-  left: 0;
-`;
-
-export const MenuList = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
-export const MenuItem = styled.li`
-  padding: 10px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #ddd;
-  }
+export const AppSideMenuContainer = styled.nav`
+    display: flex;
 `;
