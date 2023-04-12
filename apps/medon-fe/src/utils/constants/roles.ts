65bde0b2bf7b6608c4a6f248e78ba07151011fbf
eduardojigub <eduardobrunoit@gmail.com>
2023-04-12 16:49:29 -0300

    feat: create Roles constants

---
+++ apps/medon-fe/src/utils/constants/roles.ts
@@ -0,0 +1,9 @@
+export const ROLES = {
+    LOCAL: 'local',
+    REMOTE: 'remote',
+  };
+  
+export const ROLE_OPTIONS = [
+    { value: ROLES.LOCAL, label: 'Local doctor' },
+    { value: ROLES.REMOTE, label: 'Remote doctor' },
+];
