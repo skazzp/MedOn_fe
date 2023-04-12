5cf950c5cd65909d28ac71e7f9c807efc18774d6
eduardojigub <eduardobrunoit@gmail.com>
2023-04-12 16:54:12 -0300

    feat: create profileFormFields constat file

---
+++ apps/medon-fe/src/utils/constants/profileFormFields.ts
@@ -0,0 +1,8 @@
+export const FIRST_NAME = 'firstName';
+export const LAST_NAME = 'lastName';
+export const PASSWORD = 'password';
+export const EMAIL = 'email';
+export const ROLE = 'role';
+export const COUNTRY = 'country';
+export const CITY = 'city';
+export const BIRTHDAY = 'birthday';
