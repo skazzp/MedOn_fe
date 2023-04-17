a17a636c726fe78983dd9a78b869896abf2298ca
Matheus <matheus-rodrigues37@live.com>
2023-04-17 14:49:19 -0300

    MED-49 fix: min for max in validation

--- apps/medon-fe/src/validation/forgotPasswordSchema.ts
+++ apps/medon-fe/src/validation/forgotPasswordSchema.ts
@@ -8,7 +8,7 @@ export const passwordSchema = yup.object().shape({
   newPassword: yup
     .string()
     .min(6, 'validation.password.minLength')
-    .max(20, 'validation.password.minLength')
+    .max(20, 'validation.password.maxLength')
     .matches(
       /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+-])[0-9a-zA-Z!@#$%^&()_+-]{6,}$/,
       'validation.password.matches'
