4caf7d7cf63b2624f7db66c2c9734ff5e9cdd106
Matheus <matheus-rodrigues37@live.com>
2023-04-20 11:52:37 -0300

    MED-73 chore: items from the menu dropdown (NOT USABLE)

---
+++ apps/medon-fe/src/utils/constants/optionsMenuDropdown.tsx
@@ -0,0 +1,16 @@
+import { MenuProps } from 'antd';
+
+export const items: MenuProps['items'] = [
+  {
+    label: '1st menu item',
+    key: '0',
+  },
+  {
+    label: '2nd menu item',
+    key: '1',
+  },
+  {
+    label: '3rd menu item',
+    key: '2',
+  },
+];
