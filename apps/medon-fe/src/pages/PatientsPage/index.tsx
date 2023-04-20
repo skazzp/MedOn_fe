caf790f0363ba9b5a75ed4487ad3736462be02c9
Matheus <matheus-rodrigues37@live.com>
2023-04-20 11:49:59 -0300

    MED-73 feat: add patient list structure for nest routes

---
+++ apps/medon-fe/src/pages/PatientsPage/index.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Navigation from 'components/Navigation';
+import { Wrapper, Container } from 'pages/PatientsPage/styles';
+
+export function PatientsPage() {
+  return (
+    <Wrapper>
+      <Navigation />
+      <Container>
+        <Outlet />
+      </Container>
+    </Wrapper>
+  );
+}
