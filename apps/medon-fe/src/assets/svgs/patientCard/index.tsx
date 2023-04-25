2ba72f9b289dc8b17cfc9bcf2499816ebfeacaf9
Matheus <matheus-rodrigues37@live.com>
2023-04-25 15:44:03 -0300

    MED-80 chore: export from same file

---
+++ apps/medon-fe/src/assets/svgs/patientCard/index.tsx
@@ -0,0 +1,8 @@
+import { ReactComponent as Age } from './age.svg';
+import { ReactComponent as Location } from './location.svg';
+import { ReactComponent as Mail } from './mail.svg';
+import { ReactComponent as MaleSex } from './maleSex.svg';
+import { ReactComponent as Phone } from './phone.svg';
+import { ReactComponent as Edit } from './edit.svg';
+
+export { Age, Location, Mail, MaleSex, Phone, Edit };
