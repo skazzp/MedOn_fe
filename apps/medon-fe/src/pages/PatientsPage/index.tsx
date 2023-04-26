b8b5ac46bf400b61d67b71ce6152cfb3e4a82f89
Matheus <matheus-rodrigues37@live.com>
2023-04-25 22:36:09 -0300

    MED-80 fix: duplication of navigation

--- apps/medon-fe/src/pages/PatientsPage/index.tsx
+++ apps/medon-fe/src/pages/PatientsPage/index.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Navigation from 'components/Navigation';
 import { Wrapper, Container } from 'pages/PatientsPage/styles';
 
 export function PatientsPage() {
   return (
     <Wrapper>
-      <Navigation />
       <Container>
         <Outlet />
       </Container>
