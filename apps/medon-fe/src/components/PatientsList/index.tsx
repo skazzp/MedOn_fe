7e4de647ec43de701c5793a722985557912bf78d
WhiteAdam <jurchenko.a@gmail.com>
2023-04-28 12:23:10 +0200

    feat: add and integrate pagination bar

--- apps/medon-fe/src/components/PatientsList/index.tsx
+++ apps/medon-fe/src/components/PatientsList/index.tsx
@@ -3,12 +3,12 @@ import { useTheme } from 'styled-components';
 
 import LinkHome from 'components/LinkHome';
 import PatientListCard from 'components/PatientListCard';
-import { Button } from 'antd';
+import { Button, Pagination } from 'antd';
 
 import { ReactComponent as Plus } from 'assets/svgs/plus_listcard.svg';
 
 import { useGetPatientsQuery } from 'redux/api/patientApi';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Spinner from 'components/Spinner';
 import { Choose, Content, StyledSearch, Wrapper } from './styles';
 
@@ -24,6 +24,11 @@ export default function PatientsList() {
     limit,
     searchPhrase,
   });
+
+  function handlePaginationChange(page: number, pageSize: number): void {
+    setPage(page);
+    setLimit(pageSize);
+  }
 
   return (
     <Content>
@@ -48,21 +53,32 @@ export default function PatientsList() {
       ) : (
         <Wrapper>
           {data && data.total > 0 ? (
-            data.patients.map((patient) => (
-              <PatientListCard
-                id={patient.id}
-                key={patient.id}
-                firstName={patient.firstName}
-                lastName={patient.lastName}
-                gender={patient.gender}
-                dateOfBirth={patient.dateOfBirth.toString()}
-                overview={patient.overview ? patient.overview : undefined}
+            <>
+              {data.patients.map((patient) => (
+                <PatientListCard
+                  id={patient.id}
+                  key={patient.id}
+                  firstName={patient.firstName}
+                  lastName={patient.lastName}
+                  gender={patient.gender}
+                  dateOfBirth={patient.dateOfBirth.toString()}
+                  overview={patient.overview ? patient.overview : undefined}
+                />
+              ))}
+              <Pagination
+                current={page}
+                pageSize={limit}
+                total={data.total}
+                showSizeChanger={true}
+                defaultCurrent={1}
+                defaultPageSize={5}
+                pageSizeOptions={[5, 10, 20]}
+                onChange={handlePaginationChange}
               />
-            ))
+            </>
           ) : (
             <h4>{t('patient-list.no-data')}</h4>
           )}
-          <Button onClick={() => setPage(page + 1)}>Load More</Button>
         </Wrapper>
       )}
     </Content>
