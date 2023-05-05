88bcb6eaa37ca4dc4c5b10acb5cdf4ccba6f9703
skazzp <skazzp@gmail.com>
2023-05-05 16:03:41 +0300

    change validation message

--- apps/medon-fe/src/validation/selectHourRangeSchema.ts
+++ apps/medon-fe/src/validation/selectHourRangeSchema.ts
@@ -2,5 +2,8 @@ import * as yup from 'yup';
 
 export const hoursSchema = yup.object().shape({
   start: yup.number().required(),
-  end: yup.number().moreThan(yup.ref('start'), 'Wrong time').required(),
+  end: yup
+    .number()
+    .moreThan(yup.ref('start'), 'availability.validationTime')
+    .required(),
 });
