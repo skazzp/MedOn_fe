68ceb0d4206fa68dadda12fcc2b47b979dc6af87
Matheus <matheus-rodrigues37@live.com>
2023-05-13 22:26:31 -0300

    MED-117 chore: constants and mock data

--- apps/medon-fe/src/utils/constants/dateFormat.ts
+++ apps/medon-fe/src/utils/constants/dateFormat.ts
@@ -1,1 +1,2 @@
-export const DATE_FORMAT_REG = 'DD/MM/YYYY'; 
+export const DATE_FORMAT_REG = 'DD/MM/YYYY';
+export const dayAndTimeHourFormat = 'DD/MM/YYYY HH:mm';
---
+++ apps/medon-fe/src/utils/mock/patientCalendar.ts
@@ -0,0 +1,103 @@
+import { Event } from 'react-big-calendar';
+
+export const eventsCard: Event[] = [
+  {
+    title: 'Event 1',
+    start: new Date('2023-05-15T10:00:00'),
+    end: new Date('2023-05-15T11:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 2',
+    start: new Date('2023-05-15T11:00:00'),
+    end: new Date('2023-05-15T12:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: true,
+    },
+  },
+  {
+    title: 'Event 3',
+    start: new Date('2023-05-15T12:00:00'),
+    end: new Date('2023-05-15T13:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 11',
+    start: new Date('2023-05-15T13:00:00'),
+    end: new Date('2023-05-15T14:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 4',
+    start: new Date('2023-05-16T10:00:00'),
+    end: new Date('2023-05-16T11:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 5',
+    start: new Date('2023-05-16T11:00:00'),
+    end: new Date('2023-05-16T12:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: true,
+    },
+  },
+  {
+    title: 'Event 6',
+    start: new Date('2023-05-16T12:00:00'),
+    end: new Date('2023-05-16T13:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 7',
+    start: new Date('2023-05-17T10:00:00'),
+    end: new Date('2023-05-17T11:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: false,
+    },
+  },
+  {
+    title: 'Event 8',
+    start: new Date('2023-05-17T11:00:00'),
+    end: new Date('2023-05-17T12:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: true,
+    },
+  },
+  {
+    title: 'Event 9',
+    start: new Date('2023-05-17T12:00:00'),
+    end: new Date('2023-05-17T13:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: true,
+    },
+  },
+  {
+    title: 'Event 10',
+    start: new Date('2023-05-18T10:00:00'),
+    end: new Date('2023-05-18T11:00:00'),
+    resource: {
+      link: 'https://www.google.com',
+      isColor: true,
+    },
+  },
+];
