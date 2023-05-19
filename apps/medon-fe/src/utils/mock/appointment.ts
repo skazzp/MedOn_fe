53eb758c68bb8b793c9107dac5531f2ebe258638
Matheus <matheus-rodrigues37@live.com>
2023-05-19 12:42:28 -0300

    MED-149 feat: i18n and mock

---
+++ apps/medon-fe/src/utils/mock/appointment.ts
@@ -0,0 +1,106 @@
+import { IAppointmentsCardProps } from 'components/AppointmentsCard/types';
+
+export const appointmentCardMock: IAppointmentsCardProps[] = [
+  {
+    id: 123,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    // remoteDoctor: {
+    //   lastName: 'Smith',
+    // },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+  {
+    id: 124,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    remoteDoctor: {
+      lastName: 'Smith',
+    },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+  {
+    id: 125,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    // remoteDoctor: {
+    //   lastName: 'Smith',
+    // },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+  {
+    id: 126,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    remoteDoctor: {
+      lastName: 'Smith',
+    },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+  {
+    id: 127,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    // remoteDoctor: {
+    //   lastName: 'Smith',
+    // },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+  {
+    id: 128,
+    link: 'http://example.com',
+    startTime: new Date('2022-03-01T10:00:00Z'),
+    endTime: new Date('2022-03-01T11:00:00Z'),
+    localDoctorId: 456,
+    remoteDoctor: {
+      lastName: 'Smith',
+    },
+    patient: {
+      dateOfBirth: new Date('1980-01-01'),
+      firstName: 'John',
+      lastName: 'Doe',
+      gender: 'male',
+      overview: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    },
+  },
+];
