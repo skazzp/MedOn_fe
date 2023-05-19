df8ad0b4bb852fb59a4c7beee1a8cf60bf48baed
Matheus <matheus-rodrigues37@live.com>
2023-05-19 12:41:48 -0300

    MED-149 fix: redo dashboard

--- apps/medon-fe/src/components/Dashboard/index.tsx
+++ apps/medon-fe/src/components/Dashboard/index.tsx
@@ -2,28 +2,36 @@ import { useTranslation } from 'react-i18next';
 import { getUserSelector } from 'redux/features/userSlice/userSelectors';
 import { useAppSelector } from 'redux/hooks';
 
-import { Container } from 'components/Dashboard/styles';
+import { AppointmentContainer, Container } from 'components/Dashboard/styles';
 import WithoutAppointments from 'components/WithoutAppointments';
 import Attention from 'components/common/Attention';
 import AppointmentsScore from 'components/AppointmentsScore';
-import AppointmentsList from 'components/AppointmentsList';
-import { patientList } from 'utils/mock/patientList';
+import { AppointmentsCard } from 'components/AppointmentsCard';
+
+import { appointmentCardMock } from 'utils/mock/appointment';
 
 export default function Dashboard() {
   const user = useAppSelector(getUserSelector);
   const { t } = useTranslation();
-  const hasAppointments = patientList && patientList.length > 0;
 
   return (
     <Container>
       <h1>
         {t('dashboard.welcome')}, Dr. {`${user.lastName || 'Anonymous'}`}
       </h1>
-      {hasAppointments ? (
+      {appointmentCardMock.length ? (
         <>
           <Attention />
-          <AppointmentsScore />
-          <AppointmentsList />
+          <AppointmentsScore quantity={appointmentCardMock.length} />
+          <AppointmentContainer>
+            {appointmentCardMock.map((appointment) => (
+              <AppointmentsCard
+                key={appointment.id}
+                isAssigned={appointment.remoteDoctor !== undefined}
+                {...appointment}
+              />
+            ))}
+          </AppointmentContainer>
         </>
       ) : (
         <WithoutAppointments />
