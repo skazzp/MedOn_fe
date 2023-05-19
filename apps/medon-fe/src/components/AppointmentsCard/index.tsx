2e68ad915951645764509b6a958ddb0a7bedfd55
Matheus <matheus-rodrigues37@live.com>
2023-05-19 15:00:51 -0300

    MED-149 chore: i18n

--- apps/medon-fe/src/components/AppointmentsCard/index.tsx
+++ apps/medon-fe/src/components/AppointmentsCard/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
 import { Modal } from 'antd';
 import { useTheme } from 'styled-components';
@@ -7,6 +8,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 
 import { ReactComponent as Profile } from 'assets/svgs/profile_listcard.svg';
 import { ReactComponent as Camera } from 'assets/images/Camera.svg';
+import { ReactComponent as Clock } from 'assets/images/Time.svg';
 
 import Button from 'components/Button';
 import { ShowMore } from 'components/ShowMore';
@@ -15,9 +17,11 @@ import { InputAntD } from 'components/common';
 import { useModal } from 'hooks/useModal';
 
 import { getAgeByDateOfBirth } from 'utils/functions/getAgeByDateOfBirth';
+import { timeFormat } from 'utils/constants';
 
 import { addLinkSchema } from 'validation/addLinkDashBoard';
 
+import { AddLink, IAppointmentsCardProps } from './types';
 import {
   Body,
   Container,
@@ -27,12 +31,14 @@ import {
   Number,
   Patient,
   RemoteAssign,
+  Time,
 } from './styles';
-import { AddLink, IAppointmentsCardProps } from './types';
 
 export function AppointmentsCard({
   id,
   link,
+  startTime,
+  endTime,
   patient,
   isLinkAdded,
   remoteDoctor,
@@ -40,68 +46,77 @@ export function AppointmentsCard({
 }: IAppointmentsCardProps) {
   const theme = useTheme();
   const { t } = useTranslation();
-
-  const { hideModal, isVisible, showModal } = useModal(false);
-
   const { handleSubmit, control } = useForm<AddLink>({
     resolver: yupResolver(addLinkSchema),
   });
+
+  const { hideModal, isVisible, showModal } = useModal(false);
 
   const addLinkSubmit: SubmitHandler<AddLink> = () => {
     // TODO: Integration add Link to DashBoard
   };
 
   return (
-    <Container isLinkAdded={isLinkAdded} {...rest}>
-      <Header>
-        <Info>
-          <Number>
-            {t('appointment.preffix-id')} {id}
-          </Number>
-          <Patient>
-            <Name>
-              {patient?.firstName?.charAt(0)} {patient?.lastName}
-            </Name>
-            {patient?.gender},
-            {getAgeByDateOfBirth(String(patient?.dateOfBirth))}{' '}
-            {t('appointment.suffix-age')}
-          </Patient>
-        </Info>
-        <RemoteAssign>
-          {isLinkAdded && link && (
-            <Link to={link}>
-              <Camera />
-            </Link>
+    <>
+      <Time>
+        <Clock />
+        {dayjs(startTime).format(timeFormat)} -{' '}
+        {dayjs(endTime).format(timeFormat)}
+      </Time>
+      <Container isLinkAdded={isLinkAdded} {...rest}>
+        <Header>
+          <Info>
+            <Number>
+              {t('appointment.preffix-id')} {id}
+            </Number>
+            <Patient>
+              <Name>
+                {patient?.firstName?.charAt(0)} {patient?.lastName}
+              </Name>
+              {patient?.gender},{' '}
+              {getAgeByDateOfBirth(String(patient?.dateOfBirth))}{' '}
+              {t('appointment.suffix-age')}
+            </Patient>
+          </Info>
+          <RemoteAssign>
+            {!isLinkAdded && (
+              <Link to={String(link)}>
+                <Camera />
+              </Link>
+            )}
+            {t('appointment.preffix-remote')}
+            <strong>
+              {t('appointment.prefix-doctor')} {remoteDoctor?.lastName}
+            </strong>
+          </RemoteAssign>
+          {isLinkAdded && (
+            <Button
+              textcolor={theme.colors.blue_500}
+              bgcolor={theme.colors.blue_100}
+              onClick={() => showModal()}
+            >
+              {t('appointment.add-link')}
+            </Button>
           )}
-          Remote -
-          <strong>
-            {t('appointment.prefix-doctor')} {remoteDoctor?.lastName}
-          </strong>
-        </RemoteAssign>
-        {!isLinkAdded && (
-          <Button
-            textcolor={theme.colors.blue_500}
-            bgcolor={theme.colors.blue_100}
-            onClick={() => showModal()}
-          >
-            Add Link
-          </Button>
-        )}
-        <Link to="#">
-          <Profile />
-        </Link>
-      </Header>
-      <Body>
-        <ShowMore text={patient?.overview} prefix="Overview: " />
-      </Body>
-      <Modal
-        title="Add Link to Zoom Call"
-        open={isVisible}
-        onOk={handleSubmit(addLinkSubmit)}
-        onCancel={hideModal}
-      >
-        <InputAntD control={control} name="link" />
-      </Modal>
-    </Container>
+          <Link to="#">
+            <Profile />
+          </Link>
+        </Header>
+        <Body>
+          <ShowMore
+            text={patient?.overview}
+            prefix={`${t('appointment.preffix-overview')}`}
+          />
+        </Body>
+        <Modal
+          title={t('appointment.modal-title')}
+          open={isVisible}
+          onOk={handleSubmit(addLinkSubmit)}
+          onCancel={hideModal}
+        >
+          <InputAntD control={control} name="link" />
+        </Modal>
+      </Container>
+    </>
   );
 }
