78f8adef2c9851e755b345e00d776a87bbadc7c2
Matheus <matheus-rodrigues37@live.com>
2023-05-24 11:44:40 -0300

    MED-112 chore: add constants and utility function

--- apps/medon-fe/src/utils/constants/pagination.ts
+++ apps/medon-fe/src/utils/constants/pagination.ts
@@ -2,3 +2,6 @@ export const defaultPage = 1;
 export const defaultPageSize = 5;
 export const pageSizeOptions = [5, 10, 20];
 export const defaultOrder = 'DESC';
+export const defaultPastLimit = 100;
+export const defaultLimit = 5;
+export const defaultOffset = 0;
