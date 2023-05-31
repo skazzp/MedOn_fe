11238a5ffd66ced4814e39b36e9cc8c788d9d9a5
WhiteAdam <jurchenko.a@gmail.com>
2023-05-31 21:35:34 +0200

    fix: create separate file for help internationalization

--- apps/medon-fe/src/pages/HelpPage/index.tsx
+++ apps/medon-fe/src/pages/HelpPage/index.tsx
@@ -1,66 +1,26 @@
 import { Collapse } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { getHelpContent } from 'translation/getHelpContent';
 import { Wrapper } from './styles';
 
 const { Panel } = Collapse;
 
 export function HelpPage() {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+  const content = getHelpContent(i18n.language);
+
   return (
     <div>
       <Wrapper>
         <h1>{t('help.title')}</h1>
         <Collapse defaultActiveKey={['1']}>
-          <Panel header={t('help.headers.1')} key="1">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.1') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.2')} key="2">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.2') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.3')} key="3">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.3') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.4')} key="4">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.4') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.5')} key="5">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.5') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.6')} key="6">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.6') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.7')} key="7">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.7') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.8')} key="8">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.8') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.9')} key="9">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.9') }}
-            />
-          </Panel>
-          <Panel header={t('help.headers.10')} key="10">
-            <div
-              dangerouslySetInnerHTML={{ __html: t('help.descriptions.10') }}
-            />
-          </Panel>
+          {content.map((section) => (
+            <Panel header={section.title} key={section.id}>
+              <div
+                dangerouslySetInnerHTML={{ __html: section.description }}
+              ></div>
+            </Panel>
+          ))}
         </Collapse>
       </Wrapper>
     </div>
