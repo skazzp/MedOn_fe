15125a5a874c5d3f7262b64980f055cac0e16bd6
WhiteAdam <jurchenko.a@gmail.com>
2023-05-31 16:51:06 +0200

    fix: select > div

--- apps/medon-fe/src/pages/HelpPage/index.tsx
+++ apps/medon-fe/src/pages/HelpPage/index.tsx
@@ -12,52 +12,52 @@ export function HelpPage() {
         <h1>{t('help.title')}</h1>
         <Collapse defaultActiveKey={['1']}>
           <Panel header={t('help.headers.1')} key="1">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.1') }}
             />
           </Panel>
           <Panel header={t('help.headers.2')} key="2">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.2') }}
             />
           </Panel>
           <Panel header={t('help.headers.3')} key="3">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.3') }}
             />
           </Panel>
           <Panel header={t('help.headers.4')} key="4">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.4') }}
             />
           </Panel>
           <Panel header={t('help.headers.5')} key="5">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.5') }}
             />
           </Panel>
           <Panel header={t('help.headers.6')} key="6">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.6') }}
             />
           </Panel>
           <Panel header={t('help.headers.7')} key="7">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.7') }}
             />
           </Panel>
           <Panel header={t('help.headers.8')} key="8">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.8') }}
             />
           </Panel>
           <Panel header={t('help.headers.9')} key="9">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.9') }}
             />
           </Panel>
           <Panel header={t('help.headers.10')} key="10">
-            <section
+            <div
               dangerouslySetInnerHTML={{ __html: t('help.descriptions.10') }}
             />
           </Panel>
