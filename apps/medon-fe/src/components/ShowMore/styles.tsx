f88edee075c935134dfd6ce4697b744a96937468
SoulOfDarknes <ve4nuy@rambler.ru>
2023-06-04 23:19:01 +0300

    fixed patient cart info

--- apps/medon-fe/src/components/ShowMore/styles.tsx
+++ apps/medon-fe/src/components/ShowMore/styles.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  word-break: break-word;
 
   > button {
     background-color: transparent;
