063007a5ad0388f994d7a4fb86dc3b367aef3eff
Matheus <matheus-rodrigues37@live.com>
2023-06-07 00:44:01 -0300

    MED-194 feat: add modal to remove future appointment

---
+++ apps/medon-fe/src/components/AppointmentsCardRemoveModal/index.tsx
@@ -0,0 +1,42 @@
+import { Modal } from 'antd';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+
+import { useDeleteAppointmentMutation } from 'redux/api/appointmentsApi';
+
+import { toastConfig } from 'utils/toastConfig';
+
+import { AppointmentsCardRemoveModalProps } from './types';
+
+export function RemoteAppointmentModal({
+  id,
+  hideRemoveModal,
+  isRemoveVisible,
+}: AppointmentsCardRemoveModalProps) {
+  const { t } = useTranslation();
+
+  const [sendData] = useDeleteAppointmentMutation();
+
+  const handleDeleteAppointment = async () => {
+    await sendData({ id })
+      .unwrap()
+      .then(() => {
+        toast.success(t('appointment.remove-modal-success'), toastConfig);
+        hideRemoveModal();
+      })
+      .catch(() => {
+        toast.error(t('appointment.remove-modal-error'), toastConfig);
+      });
+  };
+
+  return (
+    <Modal
+      title={t('appointment.remove-modal-title')}
+      open={isRemoveVisible}
+      onOk={handleDeleteAppointment}
+      onCancel={hideRemoveModal}
+    >
+      <span>{t('appointment.remove-modal-message')}</span>
+    </Modal>
+  );
+}
