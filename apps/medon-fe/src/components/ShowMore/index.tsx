c5637466646bee11cb69069c0637026b94e6999f
Matheus <matheus-rodrigues37@live.com>
2023-06-11 18:58:19 -0300

    MED-185 feat: add i18n and making changes to show mote

--- apps/medon-fe/src/components/ShowMore/index.tsx
+++ apps/medon-fe/src/components/ShowMore/index.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import AnimateHeight, { Height } from 'react-animate-height';
 
 import { Container, PrefixText } from './styles';
 import { IShowMoreProps } from './types';
+import { twoRowHeightPixel } from './utils/constants';
 
 export function ShowMore({
   overview,
@@ -10,23 +12,54 @@ export function ShowMore({
   prefixOverview,
   prefixLastNote,
 }: IShowMoreProps) {
-  const [isShowMore, setIsShowMore] = useState<boolean>(false);
-  const toggleReadMore = () => setIsShowMore((show) => !show);
+  const [height, setHeight] = useState<Height>(twoRowHeightPixel);
+  const [allHeight, setAllHeight] = useState<number>(0);
+  const [isButtonShow, setIsButtonShow] = useState<boolean>(false);
+
+  const animateHeightRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (animateHeightRef.current) {
+      setAllHeight(animateHeightRef.current.offsetHeight);
+    }
+  }, [animateHeightRef]);
+
+  useEffect(() => {
+    if (allHeight > twoRowHeightPixel) {
+      setHeight(twoRowHeightPixel);
+      setIsButtonShow(true);
+    } else {
+      setIsButtonShow(false);
+    }
+  }, [allHeight]);
 
   const { t } = useTranslation();
 
   return (
     <Container>
-      <p>
-        <PrefixText>{prefixOverview}</PrefixText>{' '}
-        {overview || `${t('patient-list.no-overview')}`}
-        <br />
-        <PrefixText>{prefixLastNote}</PrefixText>
-        {lastNote || `${t('patient-list.no-last-note')}`}
-      </p>
-      <button type="button" onClick={toggleReadMore}>
-        {isShowMore ? t('show.more') : t('show.less')}
-      </button>
+      <AnimateHeight height={height}>
+        <div ref={animateHeightRef}>
+          <PrefixText>{prefixOverview}</PrefixText>{' '}
+          {overview || `${t('patient-list.no-overview')}`}
+          {prefixLastNote && (
+            <>
+              <br />
+              <PrefixText>{prefixLastNote}</PrefixText>
+              {lastNote || `${t('patient-list.no-last-note')}`}
+            </>
+          )}
+        </div>
+      </AnimateHeight>
+      {isButtonShow && (
+        <button
+          type="button"
+          onClick={() =>
+            setHeight(height === twoRowHeightPixel ? 'auto' : twoRowHeightPixel)
+          }
+        >
+          {height === twoRowHeightPixel ? t('show.more') : t('show.less')}
+        </button>
+      )}
     </Container>
   );
 }
