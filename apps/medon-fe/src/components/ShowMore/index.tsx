70069555438aca92e31b9db9dab84162ecb9056d
Matheus <matheus-rodrigues37@live.com>
2023-06-11 16:16:34 -0300

    MED-185 fix: update overview

--- apps/medon-fe/src/components/ShowMore/index.tsx
+++ apps/medon-fe/src/components/ShowMore/index.tsx
@@ -1,24 +1,32 @@
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useShowMoreText } from './hooks';
+
 import { Container, PrefixText } from './styles';
 import { IShowMoreProps } from './types';
 
-export function ShowMore({ text, prefix }: IShowMoreProps) {
+export function ShowMore({
+  overview,
+  lastNote,
+  prefixOverview,
+  prefixLastNote,
+}: IShowMoreProps) {
+  const [isShowMore, setIsShowMore] = useState<boolean>(false);
+  const toggleReadMore = () => setIsShowMore((show) => !show);
+
   const { t } = useTranslation();
-  const { formatedText, handleShowToggle, isShowMorePossible, showMore } =
-    useShowMoreText(text);
 
   return (
     <Container>
       <p>
-        <PrefixText>{prefix}</PrefixText>{' '}
-        {formatedText || `${t('patient-list.no-overview')}`}
+        <PrefixText>{prefixOverview}</PrefixText>{' '}
+        {overview || `${t('patient-list.no-overview')}`}
+        <br />
+        <PrefixText>{prefixLastNote}</PrefixText>
+        {lastNote || `${t('patient-list.no-last-note')}`}
       </p>
-      {isShowMorePossible && (
-        <button type="button" onClick={handleShowToggle}>
-          {!showMore ? t('show.more') : t('show.less')}
-        </button>
-      )}
+      <button type="button" onClick={toggleReadMore}>
+        {isShowMore ? t('show.more') : t('show.less')}
+      </button>
     </Container>
   );
 }
